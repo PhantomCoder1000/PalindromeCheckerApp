ef4bb97d95cabe6715a9bf6e0376d499bec33f1f
PhantomCoder1000 <rohitraajanishlanka@gmail.com>
2026-03-02 16:37:18 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,19 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        System.out.println("Welcome to the Palindrome Checker Management System\nVersion : 1.0\nSystem initialized successfully.");
+        String input = "madam";
+        int len = input.length();
+        boolean found = true;
+        for (int i = 0; i < input.length() / 2; i++){
+            char ch1 = input.charAt(i);
+            char ch2 = input.charAt(len - 1);
+            if (ch1 == ch2){
+                len -= 1;
+            }
+            else {
+                found = false;
+                break;
+            }
+        }
+        System.out.println("Input text: madam\nIs it a Palindrome? : " + found);
     }
 }
