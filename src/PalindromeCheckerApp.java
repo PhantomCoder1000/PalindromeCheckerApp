1d612b486d4d3a6239080987c53bd12d35de9211
PhantomCoder1000 <rohitraajanishlanka@gmail.com>
2026-03-09 20:18:44 +0530

    UC5: Palindrome validation using Stack data structure (LIFO)

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,18 +1,20 @@
+import java.util.Stack;
+
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String input = "madam";
-        char[] charArray = input.toCharArray();
-        int left = 0;
-        int right = charArray.length - 1;
-        boolean isPalindrome = true;
+        String input = "noon";
+        Stack<Character> stack = new Stack<>();
 
-        while (left < right) {
-            if (charArray[left] != charArray[right]) {
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
-            left++;
-            right--;
         }
 
         System.out.println("Input text: " + input);
